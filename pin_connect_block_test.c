b5cb546e3347fc33c6a24b40e2553923477f30e8
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-12 00:26:16 +0530

    modified

---
+++ pin_connect_block_test.c
@@ -0,0 +1,13 @@
+#include "pin_connect_block.h"
+#include "pin_functions_defines.h"
+
+main(){
+    //cfg P0.0 for function 4
+    CfgPortPinFunc(0,0,RSVD_0_0);
+    //cfg P0.0 for function 3
+    CfgPortPinFunc(0,0,PWM1_0_0);
+    //cfg P0.0 for function 2
+    CfgPortPinFunc(0,0,TXDO_0_0);
+    //cfg P0.0 for function 1
+    CfgPortPinFunc(0,0,GPIO_0_0);
+}
---
+++ pin_functions_defines.h
@@ -0,0 +1,14 @@
+#ifndef __PIN_FUNCTIONS_DEFINES_H__
+#define __PIN_FUNCTIONS_DEFINES_H__
+
+#define PIN_FUNC1 0
+#define PIN_FUNC2 1
+#define PIN_FUNC3 2
+#define PIN_FUNC4 3
+
+#define GPIO_0_0 PIN_FUNC1
+#define TXDO_0_0 PIN_FUNC2
+#define PWM1_0_0 PIN_FUNC3
+#define RSVD_0_0 PIN_FUNC4
+
+#endif
