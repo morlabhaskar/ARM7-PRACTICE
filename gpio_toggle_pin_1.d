b99e92ac8e49db063e0a635b716481e6732385e3
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-11 21:54:18 +0530

    first commit

---
+++ gpio_toggle_pin_1.d
@@ -0,0 +1,4 @@
+.\gpio_toggle_pin_1.o: gpio_toggle_pin_1.c
+.\gpio_toggle_pin_1.o: C:\Keil\ARM\Inc\Philips\LPC21xx.h
+.\gpio_toggle_pin_1.o: delay.h
+.\gpio_toggle_pin_1.o: types.h
---
+++ types.h
@@ -0,0 +1,13 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+typedef unsigned char u8;
+typedef char s8;
+typedef unsigned short u16;
+typedef short s16;
+typedef unsigned int u32;
+typedef int s32;
+typedef float f32;
+typedef const char cs8;
+
+#endif
