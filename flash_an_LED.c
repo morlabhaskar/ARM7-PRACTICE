61c963704f14f2892cef2e9b871caa92821d3edf
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-12 19:50:07 +0530

    Added Files

---
+++ flash_an_LED.c
@@ -0,0 +1,20 @@
+#include <LPC21xx.h>   // Header for LPC2129 registers
+#include "delay.h"
+#define OPIN 6  // Set P0.6 as output
+
+int main(void) {
+    unsigned int i;
+    IODIR0 = (8 << OPIN);
+    for(i = 0; i < 10; i++) {
+        IOSET0 = (8 << OPIN);  // Turn ON LED
+        delay_ms(200);     // Delay 200 milli seconds
+
+        IOCLR0 = (8 << OPIN);  // Turn OFF LED
+        delay_ms(200);     // Delay 200 milli seconds
+    }
+    // Stop blinking (LED remains ON)
+    IOSET0 = (8 << OPIN);
+
+    while(1);  // End of program
+}
+
---
+++ one_by_one_active_8LEDs.c
@@ -0,0 +1,12 @@
+#include<LPC21xx.h>
+#include "delay.h"
+
+int main(){
+    unsigned int i;
+    IODIR0 = 255<<0;
+    for(i=0;i<8;i++){
+        IOSET0 = 1<<i;
+        delay_ms(300);
+    }
+    while(1);
+}
