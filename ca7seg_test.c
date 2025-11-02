6df4b317755e21e8e5246b1a29a7b6a16ca3256c
morlabhaskar <morlabhaskar306@gmail.com>
2025-11-03 00:49:15 +0530

    Add Files

---
+++ ca7seg_test.c
@@ -0,0 +1,18 @@
+#include "arduino.h"
+#include "arduino_defines.h"
+#include "types.h"
+#include "delay.h"
+#define CA7SEG_PINS 8
+cs8 segLUT[]={0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90};
+main(){
+    u32 i;
+    portMode(CA7SEG_PINS,8,OUTPUT);
+    while(1){
+        if(digitalRead(4)==0){
+					for(i=0;i<10;i++){
+            write2Pins(CA7SEG_PINS,8,segLUT[i]);
+            delay_ms(100);
+					}
+				}
+    }
+}
