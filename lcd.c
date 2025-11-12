70a775e88039c2b9ca4a80415008f5b6bbae2956
morlabhaskar <morlabhaskar306@gmail.com>
2025-11-12 10:50:29 +0530

    Add some files

--- lcd.c
+++ lcd.c
@@ -16,13 +16,90 @@ void WriteLCD(u8 byte){
     //byte between byte time
     delay_ms(2);
 }
-void Init_LCD(void);
-void CmdLCD(u8);
-void CharLCD(u8);
+void CmdLCD(u8 cmdByte){
+    //select cmd register
+    digitalWrite(LCD_RS,0);
+    //write to cmd register via data pins
+    WriteLCD(cmdByte);
+}
+void Init_LCD(void){
+    //cfg lcd data pins & 3 control pins as gpio out
+    portMode(LCD_DATA,11,OUTPUT);
+    //power on delay
+    delay_ms(15);
+    //initialize sequence start
+    CmdLCD(MODE_8BIT_1LINE);
+    delay_us(4100);
+    CmdLCD(MODE_8BIT_1LINE);
+    delay_us(100);
+    CmdLCD(MODE_8BIT_1LINE);
+    CmdLCD(MODE_8BIT_2LINE);
+    CmdLCD(DSP_ON_CUR_OFF);
+    CmdLCD(CLEAR_LCD);
+    CmdLCD(SHIFT_CUR_RIGHT);
+    //initialization sequence end
+}
 
-void StrLCD(s8*);
-void U32LCD(u32);
-void S32LCD(s32);
-void F32LCD(f32,u32);
-void BuildCGRAM(u8*,u8);
+void CharLCD(u8 asciival){
+    //select data register
+    digitalWrite(LCD_RS,1);
+    //write to DDRAM / display via data register
+    WriteLCD(asciival);
+}
+
+void StrLCD(s8 *p){
+    while(*p){
+        CharLCD(*p++);;
+    }
+}
+void U32LCD(u32 n){
+    u8 digit[10];
+    s32 i=0;
+    if(n==0) CharLCD('0');
+    else{
+        while(n>0){
+            digit[i++]=(n%10)+48;
+            n/=10;
+        }
+        for(--i;i>=0;i--){
+            CharLCD(digit[i]);
+        }
+    }
+}
+void S32LCD(s32 n){
+    if(n>0){
+        CharLCD('-');
+        n=-n;
+    }
+    U32LCD(n);
+}
+void F32LCD(f32 fn,u32 nDp){
+    u32 n,i;
+    if(fn<0.0){
+        CharLCD('-');
+        fn=-fn;
+    }
+    n=fn;
+    U32LCD(n);
+    CharLCD('.');
+    for(i=0;i<nDp;i++){
+        fn=(fn-n)*10;
+        n=fn;
+        CharLCD(n+48);
+    }
+
+}
+void BuildCGRAM(u8 *p,u8 nBytes){
+    u32 i;
+    //point to CGRAM start
+    CmdLCD(GOTO_CGRAM_START);
+    //select data register
+    digitalWrite(LCD_RS,1);
+    //write to CGRAM , required bytes for custom char
+    for(i=0;i<nBytes;i++){
+        WriteLCD(p[i]);
+    }
+    //point back DDRAM
+    CmdLCD(GOTO_LINE1_POS0);
+}
 
